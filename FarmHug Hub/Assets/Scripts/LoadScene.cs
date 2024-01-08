b6f6a86feff6837509dedd3b6748b093e045aaa0
Parpaploy <116086218+Parpaploy@users.noreply.github.com>
2024-01-08 11:52:41 +0700

    Main menu + Dialog

---
+++ FarmHug Hub/Assets/Scripts/LoadScene.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LoadScene : MonoBehaviour
+{
+    public float progress;
+
+    public void LoadSceneGame(string sceneName)
+    {
+        StartCoroutine(SceneStart(sceneName));
+    }
+
+    public IEnumerator SceneStart(string sceneName)
+    {
+        AsyncOperation asyncOperation = SceneManager.LoadSceneAsync(sceneName, LoadSceneMode.Single);
+        asyncOperation.allowSceneActivation = false;
+        while (!asyncOperation.isDone)
+        {
+            progress = Mathf.Clamp01(asyncOperation.progress / 0.9f);
+            if(progress == 1)
+            {
+                asyncOperation.allowSceneActivation = true;
+            }
+            yield return null;
+        }
+    }
+}
